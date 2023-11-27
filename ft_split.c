0744473a833ac2a436e09f1626bb537ae26917af
pokpalae <pokpalae@student.42wolfsburg.de>
2023-11-27 23:49:40 +0100

    mum died less motivated

--- ft_split.c
+++ ft_split.c
@@ -6,86 +6,103 @@
 /*   By: pokpalae <pokpalae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 12:25:13 by pokpalae          #+#    #+#             */
-/*   Updated: 2023/11/21 23:20:21 by pokpalae         ###   ########.fr       */
+/*   Updated: 2023/11/27 23:49:11 by pokpalae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n);
-char	**ft_split(char const *s, char c);
+char		**ft_split(char const *s, char c);
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+static char	**free_array(char **ptr, int i)
 {
-	unsigned int	i;
-
-	i = 0;
-	while (i < n && src[i] != '\0')
+	while (i > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		i--;
+		free(ptr[i]);
 	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return (dest);
+	free(ptr);
+	return (NULL);
 }
 
-char	**ft_split(char const *s, char c)
+static int	no_substrings(char const *str, char c)
 {
-	if (s == ((void *)0))
-		return ((void *)0);
+	int	i;
+	int	count;
 
-	size_t no_of_substring;
-	size_t i;
-
-	no_of_substring = 0;
 	i = 0;
-
-	while (s[i] != '\0')
+	count = 0;
+	while (str[i] != '\0')
 	{
-		if (s[i] != c)
+		if (str[i] == c)
+			i++;
+		else
 		{
-			no_of_substring++;
-			while (s[i] != c && s[i] != '\0')
+			count++;
+			while (str[i] != '\0' && str[i] != c)
 			{
 				i++;
 			}
 		}
-		i++;
 	}
-	char **result = (char **)malloc((no_of_substring + 1) * sizeof(char *));
-	if (result == ((void *)0))
-		return ((void *)0);
-	size_t sub_str_idx;
-	sub_str_idx = 0;
-	while (s[i] != '\0')
+	return (count);
+}
+
+static char	*put_string(char *word, char const *s, int i, int word_len)
+{
+	int	j;
+
+	j = 0;
+	while (word_len > 0)
 	{
-		if (s[i] != c)
-		{
-			size_t sub_str_len;
-			sub_str_len = 0;
-			while (s[i + sub_str_len] != c && s[i + sub_str_len] != '\0')
-			{
-				sub_str_len++;
-			}
-			result[sub_str_idx] = (char *)malloc((sub_str_len + 1)
-				* sizeof(char));
-			if (result[sub_str_idx] == ((void *)0))
-			{
-				size_t j;
-				j = 0;
-				while (j < sub_str_idx)
-				{
-					free(result[j]);
-					j++;
-				}
-				free(result);
-				return ((void *));
-			}
-			ft_strncpy(result[sub_str_idx], s + i, sub_str_len);
-		}
+		word[j] = s[i - word_len];
+		j++;
+		word_len--;
 	}
+	word[j] = '\0';
+	return (word);
+}
+
+static char	**split_strings(char const *s, char c, char **s2, int no_of_words)
+{
+	int	i;
+	int	word;
+	int	word_len;
+
+	i = 0;
+	word = 0;
+	word_len = 0;
+	while (word < no_of_words)
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
+		{
+			i++;
+			word_len++;
+		}
+		s2[word] = (char *)malloc((word_len + 1) * sizeof(char));
+		if (!s2[word])
+			return (free_array(s2, word));
+		put_string(s2[word], s, i, word_len);
+		word_len = 0;
+		word++;
+	}
+	s2[word] = ((void *)0);
+	return (s2);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char			**s2;
+	unsigned int	no_of_words;
+
+	if (!s)
+		return ((void *)0);
+	no_of_words = no_substrings(s, c);
+	s2 = (char **)malloc((no_of_words + 1) * sizeof(char *));
+	if (!s2)
+		return ((void *)0);
+	s2 = split_strings(s, c, s2, no_of_words);
+	return (s2);
 }
