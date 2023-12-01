8fcdca4fffe5fc3cb35144659c0aad58bde25940
pokpalae <pokpalae@student.42wolfsburg.de>
2023-12-01 20:29:02 +0100

    lets try some bonus

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: pokpalae <pokpalae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 13:04:23 by pokpalae          #+#    #+#             */
-/*   Updated: 2023/12/01 17:41:04 by pokpalae         ###   ########.fr       */
+/*   Updated: 2023/12/01 19:36:44 by pokpalae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*ptr;
 	size_t	i;
 
-	if (size && count > SIZE_MAX / size)
+	if (size != 0 && count != 0 && count > SIZE_MAX / size)
 	{
 		return ((void *)0);
 	}
